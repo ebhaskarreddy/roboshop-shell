c1b4ade35ece715f6444571a0635e27701527b72
ebhaskarreddy <ebhaskarreddy967@gmail.com>
2023-12-20 13:09:10 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -9,25 +9,30 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
 VALIDATE(){
-  if [ $1 -ne 0 ]
-  then
-    echo -e "$2...$R failed $N"
-  else
-    echo -e "$2..$G sucess $N"
-  fi
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
 if [ $ID -ne 0 ]
 then
-  echo "$R run the script ith root user $N"
-  exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-  echo "$G YOU ARE ROOT USER $N"
-fi
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
-VALIDATE $? "copy mongodb repo"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copied MongoDB Repo"
+
 dnf install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB"
